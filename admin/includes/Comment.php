0f01d99ecb7a37ce8638eaaa94fb830dd58c30d6
erevos13 <orfeasvou13@hotmail.com>
2017-06-18 17:22:01 +0300

    update

---
+++ admin/includes/Comment.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: erevos13
+ * Date: 31/5/2017
+ * Time: 7:08 μμ
+ */
+
+
+class Comment extends Db_object {
+
+    protected static   $db_table = "comments";
+    protected static   $db_table_fields = array('photo_id', 'author', 'body');
+    public $id;
+    public $photo_id;
+    public $author;
+    public $body;
+
+
+
+    public static function create_comment($photo_id, $author, $body){
+
+        if (!empty($photo_id) && !empty($author) && !empty($body) ){
+
+            $comment = new Comment();
+            $comment->photo_id = (int)$photo_id;
+            $comment->author = $author;
+            $comment->body = $body;
+
+
+
+            return $comment;
+
+        }else{
+            return false;
+        }
+
+
+
+    }//end create_comment
+
+
+    public static function find_the_comment($photo_id=0){
+
+        global $databases;
+
+        $sql = "SELECT * FROM ".self::$db_table;
+        $sql .=" WHERE photo_id = ".$databases->escape_string($photo_id);
+        $sql .= " ORDER BY photo_id ASC";
+
+
+        return self::find_this_query($sql) ;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+} //end of comment class
